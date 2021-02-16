8cea6878f877d2f9824591d271ea13e18f914ef2
K-Bizzle24 <kanin5665@gmail.com>
2021-02-16 14:28:05 -0600

    Added search for titled word

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -9,11 +9,13 @@ def search(word):
 	lower_word = word.lower()
 	if lower_word in data:
 		return data[word]
+	elif w.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[w.title()]
 	elif len(get_close_matches(lower_word, data.keys())) > 0:
 		TRUTH_SENTINEL = "Y"
 		FALSE_SENTINEL = "N"
 		i = 0
-		while i < len(get_close_matches(lower_word, data.keys())):
+		while i < len(get_close_matches(lower_word, data.keys())): #checks through words with a close spelling
 			close_word = get_close_matches(lower_word, data.keys())[i]
 			print("Did you mean {0} instead?".format((close_word)))
 			is_word = input("Input {0} for yes and {1} for no: ".format(TRUTH_SENTINEL, FALSE_SENTINEL))
@@ -22,9 +24,7 @@ def search(word):
 			elif is_word.upper() == FALSE_SENTINEL:
 				i += 1
 			else:
-				print("Please ensure you entered either {0} or {1}".format(TRUTH_SENTINEL, FALSE_SENTINEL))
-
-
+				print("Please ensure you entered either {0} or {1}".format(TRUTH_SENTINEL, FALSE_SENTINEL)) #Bad user input
 	else:
 		return "Please ensure you entered the correct word." #Word not found
 
